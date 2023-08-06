abd5252d9df6ab6ad1717c3c9cdb022845035803
Fadhil <fadhilsaheer@gmail.com>
2023-08-06 14:39:16 +0530

    feat: sample UI

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { Text, TouchableNativeFeedback, View } from 'react-native';
 
 export default function App() {
   return (
-    <View className='bg-red-300 flex items-center justify-center h-screen p-5'>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style='auto' />
+    <View className='bg-app-dark1 h-screen relative'>
+      <View className='w-full p-5 pt-10 bg-app-dark2'>
+        <Text className='text-app-text text-2xl font-bold'>Todo App</Text>
+      </View>
+      <TouchableNativeFeedback
+        onPress={() => {}}
+        background={TouchableNativeFeedback.Ripple('#D2D4D7', false)}
+      >
+        <View className='bg-app-accent w-16 h-16 rounded-full flex items-center justify-center absolute bottom-5 right-5'>
+          <Text className='text-app-text text-3xl'>+</Text>
+        </View>
+      </TouchableNativeFeedback>
     </View>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['App.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        app: {
+          dark1: '#0D1117',
+          dark2: '#1E2330',
+          dark3: '#2A3146',
+          dark4: '#343B50',
+          accent: '#5993E2',
+          text: '#EEF3FB',
+          text2: '#D2D4D7',
+        },
+      },
+    },
   },
   plugins: [],
 };
